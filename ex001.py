8bea96e585a736aa3a39270f04fccfd3d277d81b
Luiz-sun <luizhenriqueribeiroleite6@gmail.com>
2025-06-05 20:28:36 -0300

    Add files via upload

---
+++ ex001.py
@@ -0,0 +1,5 @@
+n1 = int(input("Digite o valor do primeiro numero"))
+n2 = int(input("Digite o valor do segundo numero"))
+soma = n1+n2
+print(f"Valor da soma é   {soma}")
+print(f"A soma entre {n1} e {n2} vale {soma}")
